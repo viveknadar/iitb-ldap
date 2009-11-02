e30b7501b20590d7e67431e4fb99365ccf131725
apnerve <apnerve@95d127a8-c79e-11de-8410-e5b1e99fc78e>
2009-11-02 11:06:49 +0000

    initial import

---
+++ class.ldap.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * iitb ldap class
+ *   this class helps to work with the ldap data easily
+ * @author apnerve <apnerve@gmail.com>
+ */
+
+class ldap {  
+  public $server = 'ldap.iitb.ac.in';
+  public $searchkey;
+  public $id;
+  
+  function __construct(argument) {
+    
+  }
+  
+  function getAllDetailsById($id) {
+    
+  }
+  
+  function search
+  
+}
